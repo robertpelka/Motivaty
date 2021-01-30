d21f7681e302978a31a892e39e87d1cfa38ed015
Robert Pelka <123455ff@gmail.com>
2021-01-30 19:06:35 +0100

    Initial Commit

---
+++ Motivaty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motivaty
+//
+//  Created by Robert Pelka on 30/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
