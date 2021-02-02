0d42ca33d3c26dc2f90c58da6ce3233df1828247
Robert Pelka <123455ff@gmail.com>
2021-02-02 21:50:07 +0100

    Add choosing emoji and reminder time

--- Motivaty/Controller/AddViewController.swift
+++ Motivaty/Controller/AddViewController.swift
@@ -5,15 +5,152 @@
 //  Created by Robert Pelka on 01/02/2021.
 //
 
+import UserNotifications
 import UIKit
 
 class AddViewController: UIViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var picker  = UIPickerView()
+    let emojis = [ "🚀", "💪", "📚", "🏋️", "🎸", "🧠", "💼", "🐶", "🐱", "🔥", "🥦", "🥗", "💻", "⏰", "🔧", "💊", "🚬", "🛏", "🔭", "🔬", "🖋"]
+    var selectedEmojiRow = 0
+    var isReminderSet = true
+    var hour = 18
+    var minute = 0
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var reminderButton: UIButton!
+    @IBOutlet weak var disableReminderButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleField.delegate = self
         addButton.layer.cornerRadius = addButton.frame.size.height/2
+        // Request authorization to display alerts
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            if let e = error {
+                print(e.localizedDescription)
+            }
+        }
+        // Dismiss iOS Keyboard when touching anywhere outside UITextField
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
     }
+    
+    func showPickerView(with tag: Int) {
+        picker = UIPickerView.init()
+        picker.delegate = self
+        picker.dataSource = self
+        picker.tag = tag
+        picker.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.95)
+        picker.contentMode = .center
+        picker.frame = CGRect.init(x: 0.0, y: 0.0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        if (tag == 0) {
+            picker.selectRow(selectedEmojiRow, inComponent: 0, animated: true)
+        }
+        if (tag == 1) {
+            picker.selectRow(hour, inComponent: 0, animated: true)
+            picker.selectRow(minute, inComponent: 1, animated: true)
+        }
+        self.view.addSubview(picker)
+    }
+    
+    @IBAction func changeEmojiPressed(_ sender: UIButton) {
+        showPickerView(with: 0)
+    }
+    
+    @IBAction func timePressed(_ sender: UIButton) {
+        showPickerView(with: 1)
+    }
+    
+    @IBAction func disableReminderPressed(_ sender: UIButton) {
+        isReminderSet = !isReminderSet
+        if (isReminderSet) {
+            let hourString = String(format: "%02d", hour)
+            let minuteString = String(format: "%02d", minute)
+            reminderButton.setTitle("Everyday \(hourString):\(minuteString)", for: .normal)
+            disableReminderButton.setTitle("Disable", for: .normal)
+        }
+        else {
+            reminderButton.setTitle("None", for: .normal)
+            disableReminderButton.setTitle("Enable", for: .normal)
+        }
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
 
+//MARK: - UITextFieldDelegate
+
+extension AddViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+            return false
+    }
+    
+}
+
+//MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+
+extension AddViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if (pickerView.tag == 0) {
+            return 1
+        }
+        else {
+            return 2
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (pickerView.tag == 0) {
+            return emojis.count
+        }
+        else {
+            if (component == 0) {
+                return 25
+            }
+            else {
+                return 60
+            }
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (pickerView.tag == 0) {
+            return emojis[row]
+        }
+        else {
+            return String(format: "%02d", row)
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (pickerView.tag == 0) {
+            selectedEmojiRow = row
+            emojiLabel.text = emojis[row]
+            pickerView.isHidden = true
+        }
+        else {
+            if (component == 0) {
+                hour = row
+            }
+            else {
+                minute = row
+                let hourString = String(format: "%02d", hour)
+                let minuteString = String(format: "%02d", minute)
+                reminderButton.setTitle("Everyday \(hourString):\(minuteString)", for: .normal)
+                pickerView.isHidden = true
+            }
+        }
+    }
+    
 }
