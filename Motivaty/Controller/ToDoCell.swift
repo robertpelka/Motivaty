62dee16963e394fa7823359b6d97e2431b5a3c2f
Robert Pelka <123455ff@gmail.com>
2021-02-08 20:58:10 +0100

    Fix Custom Cell content width and add app icon

--- Motivaty/Controller/ToDoCell.swift
+++ Motivaty/Controller/ToDoCell.swift
@@ -2,25 +2,24 @@
 //  ToDoCell.swift
 //  Motivaty
 //
-//  Created by Robert Pelka on 01/02/2021.
+//  Created by Robert Pelka on 08/02/2021.
 //
 
 import UIKit
 
 class ToDoCell: UITableViewCell {
 
-    @IBOutlet weak var bgView: UIView!
-    
-    @IBOutlet weak var emojiLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var streakLabel: UILabel!
     @IBOutlet weak var streakImage: UIImageView!
+    @IBOutlet weak var streakLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
     @IBOutlet weak var doneImage: UIImageView!
+    @IBOutlet weak var bgView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        if let view = bgView {
-            view.layer.cornerRadius = 15
+        if let background = bgView {
+            background.layer.cornerRadius = 15
         }
     }
 
