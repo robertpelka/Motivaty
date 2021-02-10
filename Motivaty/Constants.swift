ff03b0541652d39d069fd13d07e4b4f735160dc0
Robert Pelka <123455ff@gmail.com>
2021-02-10 16:43:02 +0100

    Add checking if habit is done and saving done date in Core Data

--- Motivaty/Constants.swift
+++ Motivaty/Constants.swift
@@ -12,4 +12,10 @@ struct K {
         static let cellIdentifier = "ReusableCell"
         static let cellNibName = "ToDoCell"
     }
+    struct Images {
+        static let dog = "doge"
+        static let happyDog = "doge2"
+        static let notDone = "Rectangle"
+        static let done = "doneRectangle"
+    }
 }
--- Motivaty/Controller/MainViewController.swift
+++ Motivaty/Controller/MainViewController.swift
@@ -24,16 +24,14 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         bottomView.layer.cornerRadius = 30
         bottomView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: K.Cell.cellNibName, bundle: nil), forCellReuseIdentifier: K.Cell.cellIdentifier)
+        
         loadHabits()
-        if habits.count == 1 {
-            firstLabel.text = "Hi Robert, there is 1 thing to do today"
-        }
-        else {
-            firstLabel.text = "Hi Robert, there are \(habits.count) things to do today"
-        }
+        checkIfHabitsDone()
+        changeTitle()
     }
     
     func loadHabits() {
@@ -46,11 +44,69 @@ class MainViewController: UIViewController {
         }
     }
     
+    func checkIfHabitsDone() {
+        for habit in habits {
+            if let lastHabitDate = habit.doneDates?.allObjects.last {
+                if let lastDate = (lastHabitDate as! HabitDate).date {
+                    let lastDateStripped = stripTime(from: lastDate)
+                    let today = stripTime(from: Date())
+                    if (lastDateStripped == today) {
+                        habit.isDone = true
+                    }
+                }
+            }
+            else {
+                habit.isDone = false
+            }
+        }
+        tableView.reloadData()
+        changeTitle()
+    }
+    
+    func stripTime(from originalDate: Date) -> Date? {
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: originalDate)
+        let date = Calendar.current.date(from: components)
+        return date
+    }
+    
+    func changeTitle() {
+        var habitsLeft = 0
+        for habit in habits {
+            if (!habit.isDone) {
+                habitsLeft += 1
+            }
+        }
+        
+        let more = (habits.count != habitsLeft) ? " more" : ""
+        
+        if (habitsLeft == 0 && habits.count > 0) {
+            firstLabel.text = "You did everything!\nNow it’s time to play!"
+            secondLabel.text = "Come back tomorrow"
+            imageView.image = UIImage(named: K.Images.happyDog)
+        }
+        else {
+            secondLabel.text = "Just do it already"
+            imageView.image = UIImage(named: K.Images.dog)
+        }
+        
+        if (habits.count == 0) {
+            firstLabel.text = "Hi Robert, add some things to do"
+            secondLabel.text = "Press plus button"
+        }
+        else if (habitsLeft == 1) {
+            firstLabel.text = "Hi Robert, there is 1\(more) thing to do today"
+        }
+        else if (habitsLeft > 1) {
+            firstLabel.text = "Hi Robert, there are \(habitsLeft)\(more) things to do today"
+        }
+    }
+    
 }
 
 //MARK: - UITableViewDataSource
 
 extension MainViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return habits.count
     }
@@ -61,17 +117,42 @@ extension MainViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.Cell.cellIdentifier, for: indexPath) as! ToDoCell
         cell.emojiLabel.text = habit.emoji
         cell.titleLabel.text = habit.title
+        cell.doneImage.image = habit.isDone ? UIImage(named: K.Images.done) : UIImage(named: K.Images.notDone)
+        
         return cell
     }
-    
     
 }
 
 //MARK: - UITableViewDelegate
 
 extension MainViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        let habit = habits[indexPath.row]
+        
+        if (!habit.isDone) {
+            let newHabitDate = HabitDate(context: self.context)
+            newHabitDate.date = Date()
+            newHabitDate.doneHabit = habit
+        }
+        else {
+            let delete = habit.doneDates?.allObjects.last as! NSManagedObject
+            context.delete(delete)
+        }
+        
+        saveContext()
+        checkIfHabitsDone()
     }
+    
+    func saveContext() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Error saving context, \(error)")
+        }
+    }
+    
 }
 
